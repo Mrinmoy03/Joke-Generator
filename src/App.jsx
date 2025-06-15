5b16a6b0017f3fd65b7c944ca84e60e5ad872740
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-06-15 11:03:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Joke from './Components/Joke'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Joke Generator Using React and Joke API</h1>
+      <Joke/>
+    </div>
+  )
+}
+
+export default App
